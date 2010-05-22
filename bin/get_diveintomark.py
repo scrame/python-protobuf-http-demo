bdf12e0316d05505f1f563ed364cad53ab5363fb
liam <liam@84143535-b54b-0410-b13e-a60e24777d11>
2010-05-22 04:18:35 +0000

    killing diveintomark
    git-svn-id: http://liamchristopher.com/svn/protocol_buffers_demo@1207 84143535-b54b-0410-b13e-a60e24777d11

--- bin/get_diveintomark.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-import httplib
-httplib.HTTPConnection.debuglevel = 1    
-
-import urllib
-data = urllib.urlopen('http://www.diveintomark.org/xml/atom.xml').read()    
-print data
